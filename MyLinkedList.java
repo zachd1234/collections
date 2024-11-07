efe4fa4fa57749f480c4d6e64f7e06b6ed8e07d1
Zach <zachderhake@gmail.com>
2024-11-07 13:05:35 -0800

    uh

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,35 +1,40 @@
+//34567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+
 import java.util.NoSuchElementException;
 
 /**
- * Write a description of class MyLinkedList here.
+ * a linked List Implementation 
  *
- * @author (your name)
- * @version (a version number or a date)* 
-
+ * @author Zach Derhake
+ * @version 10/30/24 
  */
 public class MyLinkedList<E>
 {
+    
     private Node<E> head;
     private Node<E> tail;
     private int size; 
     
-    
+    /**
+     * creates a new node and inserts it as the first element 
+     * @param the data element to be added to the start of the list  
+     */
     public void addHead(E element) {
         Node<E> newNode = new Node<E>(element);
         newNode.setNext(head);
         head = newNode;
         if (isEmpty()) { 
-            tail = head;
+            tail = head; 
         }
         size++;
     }
     
+    /**
+     * creates a new node and inserts it at the end of the list 
+     * @param the data element to be added to the end of the list 
+     */
     public void addTail(E element) {
-        
-        //confused about tail. what if the tail is the first element added.
-        //do i then also have to set the head to tail as well? 
         Node<E> newNode = new Node<E>(element);
-        
         if (isEmpty()) {
             head = newNode;
             tail = newNode;
@@ -38,35 +43,47 @@ public class MyLinkedList<E>
             tail = newNode;  
         }
         size++;
-        
     }
     
+    /**
+     * removes the first element of the list and returns it 
+     * @return the data element of the first element before it was removed 
+     * @throws NoSuchElementException if the list is empty  
+     */
     public E removeHead() {
         if (isEmpty())
         {
             throw new NoSuchElementException(); 
         } else if (size == 1) { 
-            E firstItem = head.getElement();
+            E firstItem = head.getElement(); //holds return value
             head = null;
             tail = null;
             size--;
             return firstItem;
         } else {
-        E firstItem = head.getElement();
+        E firstItem = head.getElement(); //holds return value
         head = head.getNext();
         size--;
         return firstItem;
         }
     }
     
+    /**
+     * returns the first element in the list 
+     * @return the data element of the first element in the list 
+     * @throws NoSuchElementException if the list is empty 
+     */
     public E getHead() {
-        if (isEmpty())
-        {
+        if (isEmpty()) {
             throw new NoSuchElementException(); 
         }
         return head.getElement();
     }
     
+   /**
+     *  Returns whether the list is empty 
+     *  @return whether the list is empty or not 
+     */
     public boolean isEmpty() {
         if (size <= 0) {
             return true;
@@ -75,10 +92,18 @@ public class MyLinkedList<E>
         }
     }
     
+    /**
+     * returns the amount of elements in the list 
+     * @returns the amount of elements in the list  
+     */
     public int size() {
         return size; 
     }
     
+    /**
+     * turns the list into a string and returns it 
+     * @return the list in a String format with comma seperated values  
+     */
     public String toString() 
     {
         String print = "";
@@ -114,7 +139,7 @@ public class MyLinkedList<E>
             this.element = element;
         }
         
-        private void setNext (Node<E> next){
+        private void setNext(Node<E> next) {
             this.next = next; 
         }
     }
