07ad92f67d95b679202caddaba9e943e70045b4e
Zach <zachderhake@gmail.com>
2025-01-13 09:30:58 -0800

    v

--- MyHeap.java
+++ MyHeap.java
@@ -1,7 +1,7 @@
 import java.util.NoSuchElementException;
 
 /**
- * A Min Heap Tree Implemented in an Array
+ * A Min or Max Heap Tree Implemented in an Array
  *
  * @author Zach Derhake
  * @version 1/10/25
@@ -10,13 +10,17 @@ public class MyHeap<E extends Comparable<E>>
 {
     private E[] heap; 
     private int last; //keeps track of index
+    private boolean isMinHeap;
     
     /**
-     * Initializes the Heap
+     * Constructs dynamic-sized heap as either min-heap or max-heap.
+     * 
+     * @param Boolean that sets the heap: true is min, false is max.  
      */
-    public MyHeap(){
+    public MyHeap(boolean isMinHeap){
         heap = (E[]) new Comparable[100]; 
         this.last = 0;
+        this.isMinHeap = isMinHeap;
     } 
     
      /**
@@ -43,12 +47,12 @@ public class MyHeap<E extends Comparable<E>>
     }
     
     /**
-     *  Gets Minimum Element From Heap.
+     *  Gets top element (minimum or maximum) from heap.  
      *  
-     *  @return The Minimum Element From the Heap.
+     *  @return The (Minimum or Maximum) Element From the Heap.
      *  @throws NoSuchElementException if Heap is Empty.
      */
-    public E getMin() {
+    public E getTop() {
         if (isEmpty()) {
             throw new NoSuchElementException();
         } else {
@@ -57,11 +61,11 @@ public class MyHeap<E extends Comparable<E>>
     }
     
     /**
-     * Removes and returns minimum element from heap.
+     *Removes and returns top element (minimum or maximum) from heap.
      * 
-     * @return The minimum element that was removed from heap. 
+     * @return The top element (minimum or maximum) that was removed from heap. 
      */
-    public E removeMin() {
+    public E removeTop(){
         return null; //fix this
     }
     
@@ -89,25 +93,130 @@ public class MyHeap<E extends Comparable<E>>
      * @return The string contents of the heap
      */
     public String toString() {
-        String heapStr = "Heap: ";
+        String heapStr = "";
         for (int i = 0; i < last; i++) {
-            heapStr += "Element: " + heap[i] + " Index: " + i + " ";  
+            if(i == last - 1) {
+                heapStr += heap[i];
+            } else {
+                heapStr += heap[i] + ",";  
+            }
         }
         return heapStr; 
     }
     
+    /**
+     * Prints elements as stored in the tree
+     *
+     * @param maxLabelWidth The maximum space allowed for the string form
+     *                        of the element.
+     */
+    public void printTree(int maxLabelWidth) {
+        int depth = (int) (Math.log(last + 1) / Math.log(2)) + 1;
+        int maxSize = (int) Math.pow(2, depth) - 1;
+        String[] labels = new String[maxSize];
+
+        // Build array of element labels
+        buildTreeLabels(labels, maxLabelWidth);
+
+        // Print element properly spaced
+        int fullWidth = (int) Math.pow(2, depth - 1) * (maxLabelWidth + 1);
+        for (int i = 0; i < depth; i++) {
+            String connectionsLevelStr = "";
+            String labelsLevelStr = "";
+
+            for (int j = (int) Math.pow(2, i) - 1; j < (int) Math.pow(2, i + 1) - 1; j++) {
+
+                // Process arrows for this level
+                String arrow = "  ";
+                int arrowLength = arrow.length();
+                if (labels[j] != null) {
+                    if (j % 2 == 1) { // Odd is left child
+                        arrow = " /";
+                    } else { // Even is right child
+                        arrow = "\\ ";
+                    }
+                }
+
+                // Center within maxElementWidth
+                String leftPadStr = ""; // Default
+                String rightPadStr = ""; // Default
+                int leftPadNum = (fullWidth / (int) Math.pow(2, i) - arrowLength) / 2;
+                int rightPadNum = fullWidth / (int) Math.pow(2, i) - arrowLength - leftPadNum;
+                if (leftPadNum > 0) {
+                    leftPadStr = String.format("%" + leftPadNum + "s", " ");
+                }
+                if (rightPadNum > 0) {
+                    rightPadStr = String.format("%" + rightPadNum + "s", " ");
+                }
+                connectionsLevelStr += leftPadStr + arrow + rightPadStr;
+
+                // Process elements for this level
+                int labelLength = 0;
+                if (labels[j] != null) {
+                    labelLength = labels[j].length();
+                }
+
+                // Center within maxLabelWidth
+                leftPadStr = ""; // Default
+                rightPadStr = ""; // Default
+                leftPadNum = (fullWidth / (int) Math.pow(2, i) - labelLength) / 2;
+                rightPadNum = fullWidth / (int) Math.pow(2, i) - labelLength - leftPadNum;
+                if (leftPadNum > 0) {
+                    leftPadStr = String.format("%" + leftPadNum + "s", " ");
+                }
+                if (rightPadNum > 0) {
+                    rightPadStr = String.format("%" + rightPadNum + "s", " ");
+                }
+
+                if (labels[j] != null) {
+                    labelsLevelStr += leftPadStr + labels[j] + rightPadStr;
+                } else {
+                    labelsLevelStr += leftPadStr + rightPadStr;
+                }
+            }
+
+            if (i > 0) { // Do not print arrows for root
+                System.out.println(connectionsLevelStr);
+            }
+            System.out.println(labelsLevelStr);
+        }
+    }
+
+    // build array of element strings
+    private void buildTreeLabels(String[] labels, int maxLabelWidth) {
+        for (int i = 0; i <= last; i++) {
+            labels[i] = heap[i].toString();
+            if (labels[i].length() > maxLabelWidth) {
+                labels[i] = labels[i].substring(0, maxLabelWidth);
+            }
+        }
+    }
+
+    
     public static void main() {
-        MyHeap<Integer> test = new MyHeap<Integer>(); 
-        System.out.println(test);
+        MyHeap<Integer> test = new MyHeap<Integer>(true); 
+        System.out.println("Heap " + test);
+        System.out.println("Size " + test.size());
+        
         test.add(3);
-        System.out.println(test);
+        System.out.println("Heap " + test);
+        System.out.println("Size " + test.size());
+        test.printTree(100); //doesn't work. throws an error. 
         test.add(77);
+        System.out.println("Heap " + test);
+        System.out.println("Size " + test.size());
+        test.printTree(100);
         test.add(1242);
+        System.out.println("Heap " + test);
+        System.out.println("Size " + test.size());
+        test.printTree(100);
         test.add(2);
-        System.out.println(test);
+        System.out.println("Heap " + test);
+        System.out.println("Size " + test.size());
+        test.printTree(100);
         test.add(1);
-        System.out.println(test);
-
-
+        System.out.println("Heap " + test);
+        System.out.println("Size " + test.size());
+        test.printTree(100);
     } 
 }
